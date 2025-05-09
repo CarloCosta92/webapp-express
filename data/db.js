2f83143e896a3e96861d85dfe34c9d4e49fadcb5
Carlo Costa <carlo.costa92@gmail.com>
2025-05-09 16:10:15 +0200

    feat: set env file

--- data/db.js
+++ data/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'film'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 connection.connect((err) => {
     if (err) throw err;
